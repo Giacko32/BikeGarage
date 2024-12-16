e99622a37b28f551f389aaf3ac0949feaba461a6
giacomo <giacomolombardo67@gmail.com>
2024-12-16 23:13:29 +0100

    first commit

---
+++ src/main/java/com/wsda/bikegarage/BikeGarageApplication.java
@@ -0,0 +1,13 @@
+package com.wsda.bikegarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeGarageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsda/bikegarage/BikeGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsda.bikegarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeGarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
