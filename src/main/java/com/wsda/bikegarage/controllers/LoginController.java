92d0a973aac78188195ded482ec2f42934551488
giacomo <giacomolombardo67@gmail.com>
2024-12-19 12:11:35 +0100

    commit

--- src/main/java/com/wsda/bikegarage/controllers/LoginController.java
+++ src/main/java/com/wsda/bikegarage/controllers/LoginController.java
@@ -1,13 +1,15 @@
 package com.wsda.bikegarage.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
     @PostMapping("/login")
     public String login(@RequestParam(name="username", required = true) String username, @RequestParam(name = "password", required = true) String password) {
         System.out.println(username + " " + password);
-        return "ciao";
+        return "accettazione";
     }
 }
