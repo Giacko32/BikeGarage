9364a40bc0bacb8630ac8a73d82ff290e73f2197
Gianvito12 <gianvitocognata261@gmail.com>
2024-12-24 10:48:24 +0100

    commit

--- src/main/resources/static/scripts/magazzino.js
+++ src/main/resources/static/scripts/magazzino.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(".inventory-list").on("click",".inventory-item",function() {
         $("#add-button").show()
+        $("#update").show()
         $("#add-button-ricambio").hide()
         // Ottieni l'ID del ricambio
         var partId = $(this).attr("id");
@@ -8,11 +9,13 @@ $(document).ready(function() {
         // Ottieni il nome, la quantità e il prezzo del ricambio
         var partName = $(this).find("span").eq(0).text().replace("Nome:", "").trim();
         var partPrice = $(this).find("span").eq(2).text().replace("Prezzo:", "").trim().replace("€", "");
+        var partQuantity = $(this).find("span").eq(1).text().replace("Quantità:", "").trim();
 
         // Imposta i valori negli input del form
         $("#part_id").attr("readonly",true).val(partId);
         $("#part_name").attr("readonly",true).val(partName);
         $("#part_price").attr("readonly",true).val(partPrice);
+        $("#part_quantity").attr("readonly",true).val(partQuantity);
 
         // Mostra la sezione per aggiungere il ricambio
         $(".add-part-section").show();
@@ -20,7 +23,7 @@ $(document).ready(function() {
 
     $("#add-button").click(function() {
         const id=$("#part_id").val();
-        const quantity=$("#part_quantity").val();
+        const quantity=$("#part_quantity_add").val();
         $.get("magazzino/aggiungi",{id:id,quantity:quantity},function(ricambio){
             $(".inventory-item").each(function() {
                 const id=parseInt($(this).attr("id"));
@@ -36,9 +39,11 @@ $(document).ready(function() {
     $("#crea-ricambio").click(function() {
         $("#add-button-ricambio").show()
         $("#add-button").hide()
+        $("#update").hide()
         $("#part_id").removeAttr("readonly").val("");
         $("#part_price").removeAttr("readonly").val("");
         $("#part_name").removeAttr("readonly").val("");
+        $("#part_quantity").removeAttr("readonly").val("");
         $(".add-part-section").show();
     })
 
