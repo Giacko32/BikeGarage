6a35d3c395b729404e508099d2a07e9c0fb17e7c
giacomo <giacomolombardo67@gmail.com>
2025-01-02 16:39:08 +0100

    commit

---
+++ src/main/resources/static/scripts/recupera_password.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#recuperoPassword").click(function () {
+        $("#modal").show()
+    })
+
+    $("#cancel").click(function () {
+        $("#modal").hide()
+    })
+
+})
